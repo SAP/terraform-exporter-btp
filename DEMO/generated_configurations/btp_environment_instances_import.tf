677f874e8a6116204b5cfcfb94dcfec7fcbcf47a
Christian Lechner <22294087+lechnerc77@users.noreply.github.com.>
2024-09-16 11:25:59 +0200

    refactor: finalized

--- DEMO/generated_configurations/btp_environment_instances_import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = btp_subaccount_environment_instance.cloudfoundry
-  id = "cf18dcef-5b15-4638-bd98-73f10c7f4f3a,8D363D57-86F1-4FF4-B3F8-C1FB7FFD34AC"
-}
--- DEMO/generated_configurations/subaccount_subscriptions_import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = btp_subaccount_subscription.feature_flags_dashboard
-  id = "cf18dcef-5b15-4638-bd98-73f10c7f4f3a,feature-flags-dashboard,dashboard"
-}
