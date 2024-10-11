04f850a3975aeb3ec15378dd0ccea0cef69456cf
Christian Lechner <22294087+lechnerc77@users.noreply.github.com.>
2024-10-11 15:06:13 +0200

    feat directory

--- generated_configurations_f366cc89-9fa7-42d8-a62e-84ccec2b16a1/btp_directory_role_collection_import.tf
+++
@@ -1,8 +0,0 @@
-import {
-  to = btp_directory_role_collection.directory_administrator
-  id = "f366cc89-9fa7-42d8-a62e-84ccec2b16a1,Directory Administrator"
-}
-import {
-  to = btp_directory_role_collection.directory_viewer
-  id = "f366cc89-9fa7-42d8-a62e-84ccec2b16a1,Directory Viewer"
-}
